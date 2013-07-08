2492ae67bce3303f29343e0275bd72b63e40d6dd
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-08 12:09:10 -0400

    Adding marker in center of map

--- app/assets/javascripts/nsa_snowden.js
+++ app/assets/javascripts/nsa_snowden.js
@@ -1,9 +1,37 @@
+var map;
+
   function display_map(lat, lng) {
     var mapOptions = {
       center: new google.maps.LatLng(lat, lng),
-      zoom: 12,
+      zoom: 14,
       mapTypeId: google.maps.MapTypeId.HYBRID
     };
     var canvas = $('#map-canvas')[0];
-    new google.maps.Map(canvas, mapOptions);
+    map = new google.maps.Map(canvas, mapOptions);
   }
+
+  function add_marker(lat, lng, title){
+    var latlng = new google.maps.LatLng(lat, lng);
+    new google.maps.Marker({
+      position: latlng,
+      map: map,
+      title: title
+      });
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
