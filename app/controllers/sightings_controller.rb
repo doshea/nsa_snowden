a16b4fb0b1f2c7c4994e7e797ca0c48af3c14482
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-07-10 14:16:17 -0400

    Edit and New pages, non-AJAXy

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -5,4 +5,27 @@ class SightingsController < ApplicationController
   def show
     @sighting = Sighting.find(params[:id])
   end
+  def new
+    @sighting = Sighting.new
+  end
+  def create
+    @sighting = Sighting.new(params[:sighting])
+    if @sighting.save
+      redirect_to sightings_path #redirect to index page
+    else
+      render :new
+    end
+  end
+  def edit
+    @sighting = Sighting.find(params[:id])
+  end
+  def update
+    @sighting = Sighting.find(params[:id])
+    if @sighting.update_attributes(params[:sighting])
+      redirect_to @sighting  #redirect to show page
+    else
+      render :edit
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 NsaSnowden::Application.routes.draw do
   root :to => 'sightings#index'
-  resources :sightings, only: [:show, :index]
+  resources :sightings, only: [:show, :index, :new, :edit, :create, :update]
 end
